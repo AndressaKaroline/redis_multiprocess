76e7d254f9ea2ca7c9d117a67c15f5feca194e95
Andressa Karoline <andressakdogado@gmail.com>
2019-08-28 10:03:39 -0300

    Adaptado para a tarefa

--- redis_master.py
+++ redis_master.py
@@ -1,21 +1,49 @@
+import numpy
+import random
+import time
+import logging
 from time import sleep
 from redis import Redis
 from rq import Queue
-from redis_modules import print_as_function
+from redis_modules import multiplica_linha_coluna
 
-if __name__ == "__main__":
-  print "Initializing redis master"
-  redis_conn = Redis(host='127.0.0.1',port=6379)
-  queue_jobs = Queue('my_queue', connection=redis_conn)
-  jobs = []
-  for i in range(10):
-    job = queue_jobs.enqueue(print_as_function, "realizando trabalho {0}".format(i))
-    jobs.append(job)
+def cria_matriz(linhas, colunas):
+  A = []
+  for i in range(linhas):
+    linha = []
+    for j in range(colunas):
+      linha = linha + [random.randint(1, 10)]
+    A = A + [linha]
+  return A
 
-  for job in jobs:
-    print "Trabalhos enfileirados {0}".format(len(queue_jobs))
-    while job.result is None:
-      print "O trabalho {0} ainda nao foi concluido".format(job.id)
-      sleep(2)
-      print "Resultado {0}".format(job.result)
-    print job.result
+if __name__ == '__main__':
+    print "Initializing redis master"
+    redis_conn = Redis(host='127.0.0.1',port=6379)
+    queue_jobs = Queue('my_queue', connection=redis_conn)
+    jobs = []
+
+    linhas, colunas = 3, 3
+
+    print "Gerando matrizes"
+    matrizA = [[1, 2, 3],[1, 2, 3],[1, 2, 3]] 
+    matrizB = [[1, 2, 3],[1, 2, 3],[1, 2, 3]] 
+    # matrizA = cria_matriz(linhas, colunas)
+    # matrizB = cria_matriz(linhas, colunas)
+    matrizC = numpy.zeros(shape=(linhas,colunas))
+
+    print "Multiplicando matrizes"
+    for i in range(len(matrizA)):
+        for j in range(len(matrizA[0])):
+            job = queue_jobs.enqueue(multiplica_linha_coluna, i, j, matrizA, matrizB)
+            jobs.append(job)
+
+    for job in jobs:
+      print "Trabalhos enfileirados {0}".format(len(queue_jobs))
+      while job.result is None:
+        print "O trabalho {0} ainda nao foi concluido".format(job.id)
+        sleep(2)
+        print "Resultado {0}".format(job.result[0])
+      print job.result[0]
+      matrizC[job.result[1]][job.result[2]] = job.result[0]
+
+    print("{}: Resultado: \n{}".format(time.strftime('%c'), matrizC))
--- redis_modules.py
+++ redis_modules.py
@@ -1,5 +1,8 @@
 from time import sleep
 
-def print_as_function(argument):
-  sleep(2)
-  return argument
+def multiplica_linha_coluna(i, j, matrizA, matrizB):
+    valor = 0
+    i, j
+    for k in range(len(matrizB)):
+       valor = valor + matrizA[i][k] * matrizB[k][j]
+    return valor, i, j
